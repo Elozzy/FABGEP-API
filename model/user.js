648cfbd5c34f6fdcca0023335476ac40f36ddde0
Ikxyz <40415727+Ikxyz@users.noreply.github.com>
2019-07-26 17:06:40 +0100

    update user model

--- model/user.js
+++ model/user.js
@@ -5,42 +5,39 @@ export class USER {
 
 
     constructor() {
-
+        this.id = '';
         this.firstName = '';
         this.lastName = '';
         this.email = '';
-        this.pin = '';
-        this.accountNumber = '';
-        this.state = '';
-        this.tel = '';
-        this.retypePwd = '';
         this.pwd = '';
+        this.pin = '';
+        this.purseNumber = '';
+        this.tel = '';
         this.gender = '';
         this.dateOfBirth = '';
         this.nationality = '';
         this.stateOfOrigin = '';
-        this.residenceAddress = '';
+        this.homeAddress = '';
         this.localGovernmentOfOrigin = '';
         this.cityTownOfOrigin = '';
         this.stateOfResidence = '';
         this.localGovernmentOfResidence = '';
         this.cityTownOfResidence = '';
-        this.isAdmin = '';
-        this.isAgent = '';
-        this.isOrg = '';
-        this.isDev = '';
-        this.isMember = '';
-        this.city = '';
+        this.isAdmin = false;
+        this.isAgent = false;
+        this.isOrg = false;
+        this.isDev = false;
+        this.isMember = true;
         this.country = '';
         this.zip = '';
         this.passport = '';
         this.identification = '';
         this.utilityBill = '';
-        this.uid = ''; uid
-        this.timestamp = '';
-        this.rating = '';
+        this.timestamp = Date.now();
+        this.rating = 0.0;
         this.online = '';
-        this.isVerified = '';
+        this.isVerified = false;
+        this.verificationStage = 0;
         this.latitude = '';
         this.longitude = '';
 
