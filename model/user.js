2310c989af3d09dd5cbe50485f3e794df964a71f
Ikxyz <40415727+Ikxyz@users.noreply.github.com>
2019-07-26 16:35:29 +0100

    user model

---
+++ model/user.js
@@ -0,0 +1,49 @@
+
+
+class USER {
+
+
+
+    constructor() {
+
+        this.firstName = '';
+        this.lastName = '';
+        this.residenceAddress = '';
+        this.email = '';
+        this.pin = '';
+        this.accountNumber = '';
+        this.state = '';
+        this.tel = '';
+        this.retypePwd = '';
+        this.pwd = '';
+        this.gender = '';
+        this.dateOfBirth = '';
+        this.nationality = '';
+        this.stateOfOrigin = '';
+        this.localGovernmentOfOrigin = '';
+        this.cityTownOfOrigin = '';
+        this.stateOfResidence = '';
+        this.localGovernmentOfResidence = '';
+        this.cityTownOfResidence = '';
+        this.isAdmin = '';
+        this.isAgent = '';
+        this.isOrg = '';
+        this.isDev = '';
+        this.isMember = '';
+        this.city = '';
+        this.country = '';
+        this.zip = '';
+        this.passport = '';
+        this.identification = '';
+        this.utilityBill = '';
+        this.uid = ''; uid
+        this.timestamp = '';
+        this.rating = '';
+        this.online = '';
+        this.isVerified = '';
+        this.latitude = '';
+        this.longitude = '';
+
+    }
+
+}
