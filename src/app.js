c71f330d2017ec5ffb44860a8fe6ab02242fe709
Ikxyz <40415727+Ikxyz@users.noreply.github.com>
2019-07-28 06:00:11 +0100

    add console msg on server start

--- src/app.js
+++ src/app.js
@@ -14,26 +14,26 @@ app.use(json());
 app.use(urlencoded({ extended: false }));
 
 app.use('/api/v1/', usersRouter);
-app.get('/', (request, response)=>{
+app.get('/', (request, response) => {
   return response.status(200).json({
     status: 200,
     error: 'Welcome to Farmmoni API'
   })
 })
 // catch 404 and forward to error handler
-app.use((request, response, next) =>{
+app.use((request, response, next) => {
   next(createError(404));
 });
 
 // error handler
-app.use((error, request, response, next)=> {
- 
+app.use((error, request, response, next) => {
+
   response.status(error.status || 500).json({
     status: error || 500,
     error: request.app.get('env') === 'development' ? error : {}
   })
 });
 const port = process.env.PORT || 5000;
-app.listen(port, ()=>{ `Listening to ${port}`});
+app.listen(port, () => { console.log(`Server Running:  ${port}`) });
 
 export default app;
