1a238d653cdc55af13d573465c1386dc69c5b831
Ikxyz <40415727+Ikxyz@users.noreply.github.com>
2019-08-08 21:03:14 +0100

    reduce the number of Object key check by one

--- src/validation/purse.js
+++ src/validation/purse.js
@@ -14,50 +14,87 @@ const isEmpty = value => {
     );
 };
 const isValidEmail = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,10})$/;
-const isIntegar = /^(?:[1-9]\d*|\d)$/;
+const isInteger = /^(?:[1-9]\d*|\d)$/;
+const isMoney = /^\d{0,6}(\.\d{0,2}){0,1}$/;
 const isValidAlphabet = /^[a-zA-Z ]*$/;
 const isValidName = /^[a-zA-Z]{3,15}$/;
 const isValidPassword = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
 const whiteSpace = /\s/g;
 const isBoolean = /^(true|false|1|0)$/;
 const isValidPhone = /^[0-9]{8,16}$/;
+const isAccountNumber = /^[0-9]{10}$/;
 
 
 export default class PurseValidator {
 
 
+    static userProfile(request, response, next) {
+        const { uid } = request.userData;
+        if (Object.keys(request.query).length > 1) {
+            return response.status(400).json({
+                status: false,
+                data: '',
+                message: 'Only uid required'
+            });
+        }
+        if (isEmpty(uid)) {
+            return response.status(400).json({
+                status: false,
+                data: '',
+                message: 'uid is required'
+            })
+        }
+        next();
+    }
 
     static purse(request, response, next) {
         const { uid } = request.query;
 
         if (Object.keys(request.query).length > 1) {
-            response.status(400).json({ status: false, message: 'less data required', data: '' });
+            return response.status(400).json({ status: false, message: 'less data required', data: '' });
 
         }
 
         if (isEmpty(uid)) {
-            response.status(400).json({ status: false, message: 'empty request', data: '' });
+            return response.status(400).json({ status: false, message: 'empty request', data: '' });
         }
         next();
     }
 
     static transactions(request, response, next) {
-        const { uid } = request.body;
-
-        if (Object.keys(request.body).length > 2) {
-            response.status(400).json({ status: false, message: 'uid and limit is required', data: '' })
-        }
+        const { uid } = request.userData;
+        const { limit } = request.query;
 
         if (isEmpty(uid) || isEmpty(limit)) {
-            response.status(400).json({ status: false, message: `uid and limit can't be left empty`, data: '' })
+            return response.status(400).json({ status: false, message: `uid and limit can't be left empty`, data: '' })
         }
 
-        if (isNumber(limit)) {
-            response.status(400).json({ status: false, message: `limit mush be an integer value`, data: '' });
+        if (!isNumber.test(limit)) {
+            return response.status(400).json({ status: false, message: `limit mush be an integer value`, data: '' });
         }
 
         next();
 
     }
 
+    static transfer(request, response, next) {
+        const { amount, toAccount, } = request.body;
+
+        if (Object.keys(request.body).length > 2) {
+            return response.status(400).json({ status: false, message: 'less data required', data: '' });
+        }
+
+        if (isEmpty(amount) || isEmpty(toAccount)) {
+            return response.status(400).json({ status: false, message: 'amount and to account is required', data: '' });
+        }
+
+        if (!isMoney.test(amount)) {
+            return response.status(400).json({ status: false, message: 'amount mush be a double', data: '' });
+        }
+        if (!isAccountNumber.test(toAccount)) {
+            return response.status(400).json({ status: false, message: 'invalid account number', data: '' });
+        }
+
+        next();
+    }
 }
