c1a1f6b90d546ab3a8322168c8234621558fa3b4
Ikxyz <40415727+Ikxyz@users.noreply.github.com>
2019-08-08 21:02:01 +0100

    moved userProfile route from routes/user.js to routes/purse.js for all route in user.js does not need authetication but all others under purse.js need authetication to make the code easy and clean

--- src/routes/purse.js
+++ src/routes/purse.js
@@ -4,9 +4,11 @@ import Validator from '../validation/purse';
 
 const router = Router();
 
-
+router.get('/user', Validator.userProfile, Controller.userProfile);
 router.get('/purse', Validator.purse, Controller.purse);
 router.post('/transactions', Validator.transactions, Controller.transactions);
+router.post('/transfer', Validator.transfer, Controller.transfer);
+// router.post('/deposit', Validator.deposit, Controller.deposit);
 
 
 export default router;
